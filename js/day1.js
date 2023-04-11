326a8cba35778bed44d65b0070ab971e63ed9285
Riya Chowdhury <bitturiyas@gmail.com>
2023-04-12 00:22:14 +0530

    lakhan ka bday

---
+++ js/day1.js
@@ -0,0 +1,22 @@
+// var n =2;
+// function square(num){
+//     var ans = num *num;
+//     return ans;
+
+// }
+// var square2 = square(n);
+// var square4 = square(4);
+// var a = 2
+// var b = 4
+// console.log(a/b)
+
+
+// String = "Kutti";
+// let bittu ="😂😂😂😂"
+// console.log(String + bittu)
+ 
+// String = "Namasta Javascript";
+// start = "wellcome thw world";
+
+// console.log(String)
+// console.log(start)
---
+++ js/day2..2.js
@@ -0,0 +1,24 @@
+//var x = 1;
+// let a="abc";
+// let b=20;
+//console.log(x);
+// function a(){
+//     var x = 10;
+//     console.log(x);
+//     return a;
+// }
+
+// let x = 100;
+// function (){
+    
+//     console.log(x);
+//     //return b;
+// }
+
+var p1 =4;
+var p2 = 5;
+function myFunction(p1, p2) {
+  console.log(p1,p2)
+    return p1 * p2;
+   
+  }
